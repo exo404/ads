7e22e417366c8f870d47e6cfd60c90e11dc5de27
exo404 <albertopetillo@hotmail.it>
2026-03-12 18:26:56 +0100

    feat: selection sort

---
+++ src/main/java/com/ads/sorting/SelectionSort.java
@@ -0,0 +1,81 @@
+package com.ads.sorting;
+
+import com.ads.sorting.SortAlgorithm;
+import com.ads.sorting.SortUtils;
+import com.ads.sorting.SortUtilsRandomGenerator;
+import java.util.Arrays;
+
+
+/**
+ * Generic Selection Sort algorithm.
+ *
+ * Selection sort repeatedly selects the minimum element from the unsorted portion of the array
+ * and swaps it with the first unsorted element until the entire array is sorted.
+ *
+ * Time Complexity:
+ * - Selection sort always requires O(n^2) comparisons
+ * - Best case = Average case = Worst case = O(n^2)  
+ *
+ * Space Complexity: O(1) – in-place sorting
+ *
+ * @see SortAlgorithm
+ */
+class SelectionSort implements SortAlgorithm {
+   
+    /**
+     * Sorts the given array using the Selection Sort algorithm.
+     *
+     * @param array The array to be sorted
+     * @param <T>   The type of elements in the array, which must be comparable
+     * @return The sorted array
+     */
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        return sort(array, 0, array.length);
+    }
+
+    /**
+     * Sorts the given array using the Selection Sort algorithm
+     * 
+     * @param array The array to be sorted
+     * @param <T> The type of elements in the array, which must be comparable
+     * @param startIndex First index of the sub-array to be sorted
+     * @param endIndex Last index of the sub-array to be sorted
+     * @return The sorted array
+     */
+    public <T extends Comparable<T>> T[] sort(T[] array, int startIndex, int endIndex) {
+        if (array == null || startIndex >= endIndex) {
+            return array;
+        }
+
+        for (int i = startIndex; i < endIndex - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < endIndex; j++) {
+                if (SortUtils.less(array[j], array[min])) {
+                    min = j;
+                }
+            }
+            SortUtils.swap(array, i, min);
+        }
+
+        return array;
+    }
+
+    /**
+     * Main method to test the Selection Sort algorithm.
+     *
+     * @param args Command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        final SelectionSort selectionSort = new SelectionSort();
+        final Integer[] randomArray = Arrays.stream(SortUtilsRandomGenerator.generateIntArray(10, 100))
+                .boxed()
+                .toArray(Integer[]::new);
+        System.out.println("Original array:");
+        SortUtils.print(randomArray);
+        System.out.println("Sorted array");
+        SortUtils.print(selectionSort.sort(randomArray));
+    }
+
+
+}
